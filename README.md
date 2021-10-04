80b788438d650a1223a544ba50a2c3bc0f1e2b8a
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-10-04 23:31:47 +0600

    done

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Website Link: [deVAcademy](https://dev-academydotcom.netlify.app/).
 
 ## About this site:-
 
-* 
-* 
-* 
+* This website is designed to get a devloping knowledge course,
+* Every people in the world can easily do this course online
+* Hot only for one language , this course is for every platform in programming
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,33 @@ import React from 'react';
 import './Footer.css'
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="footer">
+            <div className="footer-in">
+                <div className="get-in-touch">
+                    <h1>GET IN TOUCH</h1>
+                    <div className="address">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <p>G5 Technology JSC, Room 1101, Glory Palace Apartment, Truong Thi Ward, Vinh City, Nghe An Province, Bangladesh(42000).</p>
+                    </div>
+                    <div className="address">
+                        <i class="fas fa-phone"></i>
+                        <p>(+84) 388-969-888</p>
+                    </div>
+                    <div className="address">
+                        <i class="fas fa-envelope"></i>
+                        <p>musfiquehasan08@gmail.com</p>
+                    </div>
+
+                </div>
+                <div className="ask-qus">
+                    <h1>ASK A QUESTION</h1>
+                    <input type="text" name="" id="" placeholder="EMALL ADDRESS" />
+                    <input type="text" name="" id="" placeholder="QUESTION" />
+                    
+                </div>
+            </div>
+            <p className="footer-copyright">© 2021 deVAcademy All Rights Reserved.</p>
+
         </div>
     );
 };
